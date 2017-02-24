7abfee6ea0ab7eb6ece41b8fe15062fa82b92794
Amaury Martiny <amaury.martiny@gmail.com>
2017-02-25 00:45:21 +0100

    Add bootstrap

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,5 +1,9 @@
 import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
+
+// Import bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
@@ -8,10 +12,16 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <IndexLink to="/">Home</IndexLink>
-        {' | '}
-        <Link to="/expenses">Demo App</Link>
-        <br/>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">React-Bootstrap</Link>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem><Link to="expenses">My Expenses</Link></NavItem>
+          </Nav>
+        </Navbar>
         {this.props.children}
       </div>
     );
