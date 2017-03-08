9bff62791ecd0bea0b9db0dfa4d5c831c8d05654
Amaury Martiny <amaury.martiny@gmail.com>
2017-03-08 15:07:54 +0100

    Redo design of login page

--- client/src/containers/LoginPage/LoginPage.js
+++ client/src/containers/LoginPage/LoginPage.js
@@ -1,43 +1,100 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
+import { reduxForm, SubmissionError } from 'redux-form';
+import { push } from 'react-router-redux';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { Table } from 'react-bootstrap';
+import { Field } from 'redux-form';
+import { Button } from 'react-bootstrap';
+import { feathersAuthentication } from '../../feathers';
 
-import { expensesActions } from '../../ducks/expenses';
+// TODO Put into saga
+const handleSubmit = (values, dispatch) => new Promise((resolve, reject) => {
+  dispatch(feathersAuthentication.authenticate(
+    { type: 'local', email: values.email, password: values.password }
+  ))
+    .then(() => resolve())
+    .catch(err => console.err(err));
+});
 
-class LoginPage extends React.Component {
+
+class Form extends React.Component {
+  componentWillMount() {
+    this.props.handleLogout();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!this.props.isAuthenticated && nextProps.isAuthenticated) { // true after successful submit
+      this.props.handleRedirect();
+    }
+  }
+
   render() {
+    const { handleSubmit, pristine, reset, submitting, invalid } = this.props;
+
     return (
-      <Table striped bordered condensed hover>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Description</th>
-            <th>Comment</th>
-            <th>Amount</th>
-          </tr>
-        </thead>
-      </Table>
+      <div className="container">
+
+        <form onSubmit={handleSubmit} className="col-md-6 col-md-offset-3 text-center">
+          <h2>Sign In</h2>
+          <Field
+            name="email"
+            className="form-control"
+            component="input"
+            type="email"
+            placeholder="Email"
+          />
+          <br />
+
+          <Field
+            name="password"
+            className="form-control"
+            component="input"
+            type="password"
+            placeholder="Password"
+          />
+          <br />
+
+          <div>
+            <Button
+              className="btn-lg"
+              disabled={invalid || submitting}
+              type="submit"
+            >
+              {submitting ? 'Signing In...' : 'Sign In'}
+            </Button>
+          </div>
+
+        </form>
+      </div>
     );
   }
 }
 
-LoginPage.propTypes = {
-  actions: PropTypes.object.isRequired,
-  auth: PropTypes.array.isRequired,
-};
 
-const mapStateToProps = state => {
-  return {auth: state.auth };
-};
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isSignedIn,
+});
 
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(expensesActions, dispatch)
-  };
-}
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  handleLogout: () => {
+    dispatch(feathersAuthentication.logout());
+  },
+  handleRedirect: () => {
+    dispatch(push(ownProps.redirectTo || '/'));
+  },
+});
 
+// decorate with redux
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(LoginPage);
+)(
+  // decorate react component with redux-form
+  reduxForm({
+    form: 'Login',
+    // initialValues: { email: 'a@a.com' }, // set initialValues in mapStateToProps for dynamic data
+    // validate: usersClientValidations.signin,
+    // asyncBlurFields: ['email', 'password'],
+    // asyncValidate: (values, dispatch, props) => new Promise(...),
+    onSubmit: handleSubmit,
+  })(Form)
+);
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,6 +1,7 @@
 // Set up your root reducer here...
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { reducer as reduxFormReducer } from 'redux-form'
 import { feathersAuthentication } from '../feathers';
 import expenses from '../ducks/expenses';
 
@@ -8,7 +9,8 @@ import expenses from '../ducks/expenses';
 const rootReducer = combineReducers({
   auth: feathersAuthentication.reducer,
   expenses,
-  routing: routerReducer
+  routing: routerReducer,
+  form: reduxFormReducer
 });
 
 export default rootReducer;
