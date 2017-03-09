2b4de5c0810b567736f66a6b98081e87b6aaeedf
Amaury Martiny <amaury.martiny@gmail.com>
2017-03-09 23:20:01 +0100

    Remove seeding

--- server/src/index.js
+++ server/src/index.js
@@ -2,12 +2,12 @@
 
 const app = require('./app');
 const port = app.get('port');
-app.seed()
-  .then(() => {
+// app.seed()
+  // .then(() => {
     const server = app.listen(port);
 
     server.on('listening', () =>
       console.log(`Feathers application started on ${app.get('host')}:${port}`)
     );
-  })
-  .catch(error => console.error('Error while seeding database.', error));
+  // })
+  // .catch(error => console.error('Error while seeding database.', error));
